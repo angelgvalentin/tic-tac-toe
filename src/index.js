78732255837b976aa66d66654071a95761abd1d1
Angel Valentin <angelgvalentin7@gmail.com>
2022-05-10 11:59:39 -0500

    Done game

--- src/index.js
+++ src/index.js
@@ -25,17 +25,26 @@ const Board = () => {
         const newSquares = [...squares];
         // console.log(...squares);
         //2. Mutate the copy setting the i-th element to X
+
+        const winnerDeclared = Boolean(calculateWinner(newSquares));
+        const sqaureFilled = Boolean(newSquares[i]);
+
+        if (winnerDeclared || sqaureFilled) {
+            return;
+        }
         newSquares[i] = xIsNext ? "X" : "O";
         //3. Call the setSquares function with the mutated copy
         setSquares(newSquares);
         setXIsNext(!xIsNext);
+        calculateWinner();
     };
 
     const renderSquare = (i) => {
         return <Square value={squares[i]} onClickEvent={() => handleClickEvent(i)} />;
     };
 
-    const status = `Current Player: ${xIsNext ? "X" : "O"}`;
+    const winner = calculateWinner(squares);
+    const status = winner ? `Winner: ${winner}` : `Current Player: ${xIsNext ? "X" : "O"}`;
 
     return (
         <div className="">
@@ -75,7 +84,24 @@ root.render(
     </React.StrictMode>
 );
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], // rows
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8], //columns
+        [2, 4, 6],
+    ];
+
+    for (const line of lines) {
+        const [a, b, c] = line;
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]; //"X or "O"
+        }
+    }
+
+    return null;
+};
